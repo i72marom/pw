220e52ca2c244ed9c386c1425b52f8a0cf0e92e6
manuel mariscal <i72marom@uco.es>
2020-12-16 18:29:11 +0100

    html

---
+++ p3_html/js/muestraRegistro.js
@@ -0,0 +1,7 @@
+function muestraRegistro() {
+	var login    = document.getElementById('login-form');
+	var registro = document.getElementById('registro-form');
+
+	login.style.display="none";
+	login.style.display="block";
+}
